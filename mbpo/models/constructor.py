7b817ef4dc897b2f3f7f419480ead200dd1aefc7
frank <franktian424@qq.com>
2021-05-10 14:05:17 +0800

    before CV class

--- mbpo/models/constructor.py
+++ mbpo/models/constructor.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 
 from mbpo.models.fc import FC
 from mbpo.models.bnn import BNN
+from mbpo.models.causal_bnn import CausalBNN
+from mbpo.models.single_dim_fc import SingleDimFC
 
 def construct_model(obs_dim=11, act_dim=3, rew_dim=1, hidden_dim=200, num_networks=7, num_elites=5, session=None):
 	print('[ BNN ] Observation dim {} | Action dim: {} | Hidden dim: {}'.format(obs_dim, act_dim, hidden_dim))
@@ -19,6 +21,22 @@ def construct_model(obs_dim=11, act_dim=3, rew_dim=1, hidden_dim=200, num_networ
 	model.finalize(tf.train.AdamOptimizer, {"learning_rate": 0.001})
 	return model
 
+
+def construct_casual_model(obs_dim=11, act_dim=3, rew_dim=1, hidden_dim=200, num_networks=7, num_elites=5, session=None):
+	print('[ BNN ] Observation dim {} | Action dim: {} | Hidden dim: {}'.format(obs_dim, act_dim, hidden_dim))
+	params = {'name': 'CausalBNN', 'num_networks': num_networks, 'num_elites': num_elites, 'sess': session}
+	# num_networks是model ensemble的数量
+	casual_model = CausalBNN(params, obs_dim, rew_dim)
+
+	# 第一层必须指定input，后面层的input可以自动计算
+	casual_model.add(SingleDimFC(hidden_dim, input_dim=obs_dim + act_dim, activation="swish", weight_decay=0.000025))
+	casual_model.add(SingleDimFC(hidden_dim, activation="swish", weight_decay=0.00005))
+	casual_model.add(SingleDimFC(hidden_dim, activation="swish", weight_decay=0.000075))
+	casual_model.add(SingleDimFC(1, weight_decay=0.0001))
+	casual_model.finalize(tf.train.AdamOptimizer, {"learning_rate": 0.001})
+	return casual_model
+
+
 def format_samples_for_training(samples):
 	obs = samples['observations']
 	act = samples['actions']
@@ -30,9 +48,11 @@ def format_samples_for_training(samples):
 	outputs = np.concatenate((rew, delta_obs), axis=-1)
 	return inputs, outputs
 
+
 def reset_model(model):
 	model_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=model.name)
 	model.sess.run(tf.initialize_vars(model_vars))
+
 
 if __name__ == '__main__':
 	model = construct_model()
